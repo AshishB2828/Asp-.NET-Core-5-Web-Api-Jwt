e647cc406cd212a7b789dbe1305b1e166c1ccffd
Ashish <ashishbahuleyan34@gmail.com>
2022-01-24 17:05:11 +0530

    implement repository-pattern

---
+++ IRepository/IUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using ListingApi.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingApi.IRepository
+{
+    interface IUnitOfWork:IDisposable
+    {
+        IRepository<Country> Countries { get; }
+        IRepository<Hotel> Hotels { get; }
+        Task Save();
+    }
+}
---
+++ Repository/UnitOfWork.cs
@@ -0,0 +1,36 @@
+﻿using ListingApi.Data;
+using ListingApi.IRepository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingApi.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private readonly DatabaseContext _context;
+        private  IRepository<Country> _countries;
+        private  IRepository<Hotel> _hotels;
+        public UnitOfWork(DatabaseContext context)
+        {
+            _context = context;
+
+        }
+        public IRepository<Country> Countries => _countries ??= new Repository<Country>(_context);
+
+        public IRepository<Hotel> Hotels => _hotels ??= new Repository<Hotel>(_context);
+
+        public void Dispose()
+        {
+            _context.Dispose();
+            GC.SuppressFinalize(this);
+        }
+
+        public async Task Save()
+        {
+            await _context.SaveChangesAsync();
+        }
+        
+    }
+}
