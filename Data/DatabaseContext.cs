f0dbcfef6cd5f3d4f1ba862746af51977f28a3cb
Ashish <ashishbahuleyan34@gmail.com>
2022-01-24 16:26:07 +0530

    database-settings

---
+++ Data/DatabaseContext.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ListingApi.Data
+{
+    public class DatabaseContext:DbContext
+    {
+
+
+        public DatabaseContext(DbContextOptions options):base(options)
+        {
+
+        }
+
+        public DbSet<Country> Countries { get; set; }
+        public DbSet<Hotel> Hotels { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<Country>().HasData(
+                new Country
+                {
+                    Id = 1,
+                    Name = "CNTest-1",
+                    ShortName = "CSTest-1"
+                },
+                new Country
+                {
+                    Id = 2,
+                    Name = "CNTest-2",
+                    ShortName = "CSTest-2"
+                },
+                new Country
+                {
+                    Id = 3,
+                    Name = "CNTest-3",
+                    ShortName = "CSTest-3"
+                }
+            );
+
+            builder.Entity<Hotel>().HasData(
+                new Hotel
+                {
+                    Id = 1,
+                    Name = "HNTest-1",
+                    Address = "HATest-1",
+                    CountryId = 1,
+                    Rating = 4.5
+                },
+                new Hotel
+                {
+                    Id = 2,
+                    Name = "HNTest-2",
+                    Address = "HATest-1",
+                    CountryId = 3,
+                    Rating = 4.3
+                },
+                new Hotel
+                {
+                    Id = 3,
+                    Name = "HNTest-3",
+                    Address = "HATest-1",
+                    CountryId = 2,
+                    Rating = 4
+                }
+            );
+        }
+     }
+}
